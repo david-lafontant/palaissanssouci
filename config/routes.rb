c54b7baf3e5b15c5f7009a6c083443b290006685
david-lafontant <lafontant.david@gmail.com>
2025-02-05 14:26:50 -0500

    Setup custom routes for pages views

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  root "pages#index"
+  get "about" => "pages#about", as: :about
+  get "contact" => "pages#contact", as: :contact
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
