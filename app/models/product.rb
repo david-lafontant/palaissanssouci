70710538273457a9005945d6087a130d12783ac9
david-lafontant <lafontant.david@gmail.com>
2025-02-17 17:57:24 -0500

    Add association of product to line_item and cart, and method to handle destroy

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,17 @@
 class Product < ApplicationRecord
   belongs_to :category
   has_many_attached :pictures
+  has_many :line_items
+  has_many :carts, through: :line_items
+
+  before_destroy :ensure_not_referenced_by_any_line_item
+
+  private
+
+  def ensure_not_referenced_by_any_line_item
+    return if line_items.empty?
+
+    errors.add(:base, 'Line Items present')
+    throw :abort
+  end
 end
