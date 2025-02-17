7c5f98693aa5b1bc2e10894eee40d64546c14bc4
david-lafontant <lafontant.david@gmail.com>
2025-02-17 07:07:21 -0500

    Scaffold Cart

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_02_09_123409) do
+ActiveRecord::Schema[8.0].define(version: 2025_02_17_115227) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -60,6 +60,11 @@ ActiveRecord::Schema[8.0].define(version: 2025_02_09_123409) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_articles_on_user_id"
+  end
+
+  create_table "carts", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "categories", force: :cascade do |t|
