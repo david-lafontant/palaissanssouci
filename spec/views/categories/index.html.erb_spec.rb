e3c72838a046d0664c554954f5dafc6715f52c45
david-lafontant <lafontant.david@gmail.com>
2025-02-16 22:01:13 -0500

    Fix rubocop error

--- spec/views/categories/index.html.erb_spec.rb
+++ spec/views/categories/index.html.erb_spec.rb
@@ -17,7 +17,7 @@ RSpec.describe 'categories/index', type: :view do
   it 'renders a list of categories' do
     render
     cell_selector = 'div>p'
-    assert_select cell_selector, text: Regexp.new('Name'.to_s), count: 2
+    assert_select cell_selector, text: Regexp.new('Name'), count: 2
     assert_select cell_selector, text: Regexp.new(nil.to_s), count: 2
   end
 end
--- spec/views/products/index.html.erb_spec.rb
+++ spec/views/products/index.html.erb_spec.rb
@@ -23,9 +23,9 @@ RSpec.describe 'products/index', type: :view do
   it 'renders a list of products' do
     render
     cell_selector = 'div>p'
-    assert_select cell_selector, text: Regexp.new('Name'.to_s), count: 2
-    assert_select cell_selector, text: Regexp.new('MyText'.to_s), count: 2
-    assert_select cell_selector, text: Regexp.new('9.99'.to_s), count: 2
+    assert_select cell_selector, text: Regexp.new('Name'), count: 2
+    assert_select cell_selector, text: Regexp.new('MyText'), count: 2
+    assert_select cell_selector, text: Regexp.new('9.99'), count: 2
     assert_select cell_selector, text: Regexp.new(nil.to_s), count: 2
     assert_select cell_selector, text: Regexp.new(nil.to_s), count: 2
   end
