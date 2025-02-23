ce15bad58d63eafae9cf75ac8ac83940dca92674
david-lafontant <lafontant.david@gmail.com>
2025-02-22 19:38:32 -0500

    Add order_id and change card_id contraint in line_items table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_02_22_220256) do
+ActiveRecord::Schema[8.0].define(version: 2025_02_22_223815) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -75,13 +75,15 @@ ActiveRecord::Schema[8.0].define(version: 2025_02_22_220256) do
 
   create_table "line_items", force: :cascade do |t|
     t.bigint "product_id", null: false
-    t.bigint "cart_id", null: false
+    t.integer "cart_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "quantity", default: 1
     t.decimal "product_price", null: false
     t.decimal "subtotal", null: false
+    t.bigint "order_id"
     t.index ["cart_id"], name: "index_line_items_on_cart_id"
+    t.index ["order_id"], name: "index_line_items_on_order_id"
     t.index ["product_id"], name: "index_line_items_on_product_id"
   end
 
@@ -145,6 +147,7 @@ ActiveRecord::Schema[8.0].define(version: 2025_02_22_220256) do
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
   add_foreign_key "articles", "users"
   add_foreign_key "line_items", "carts"
+  add_foreign_key "line_items", "orders"
   add_foreign_key "line_items", "products"
   add_foreign_key "products", "categories"
 end
