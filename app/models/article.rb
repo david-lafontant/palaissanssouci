54992c32090e550b34e468a2c66275b1719af2e6
david-lafontant <lafontant.david@gmail.com>
2025-03-01 14:11:15 -0500

    Add validations to Article

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,4 +2,21 @@ class Article < ApplicationRecord
   belongs_to :user
   has_rich_text :content
   has_one_attached :poster
+
+  validates :title, :slug, presence: true, length: { in: 6..300 }, uniqueness: true
+  validates :poster, :poster_description, presence: true
+  validate :validate_poster
+  validates :content, presence: true
+
+  private
+
+  def validate_poster
+    return unless poster.attached?
+
+    errors.add(:poster, 'is too big, it shoulb be less than 1 MB') unless poster.blob.byte_size <= 1.megabyte
+    acceptable_types = ['image/jpeg', 'image/png']
+    return if acceptable_types.include?(poster.content_type)
+
+    errors.add(:poster, 'must be a JPEG or PNG')
+  end
 end
