8f70a9e2cd1e16541e1eef94caf0060681261e50
david-lafontant <lafontant.david@gmail.com>
2025-03-01 14:11:42 -0500

    Add validations to Category

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,4 +1,6 @@
 class Category < ApplicationRecord
   has_one_attached :picture
   has_many :products
+  validates_associated :products
+  validates :name, presence: true, uniqueness: true, length: { in: 3..20 }
 end
