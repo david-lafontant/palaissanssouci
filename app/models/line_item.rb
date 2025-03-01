43532f524acc6581e400156832edd408f5498dea
david-lafontant <lafontant.david@gmail.com>
2025-03-01 14:11:55 -0500

    Add validations to LineItem

--- app/models/line_item.rb
+++ app/models/line_item.rb
@@ -2,4 +2,7 @@ class LineItem < ApplicationRecord
   belongs_to :cart, optional: true
   belongs_to :order, optional: true
   belongs_to :product
+
+  validates :quantity, :product_price, :subtotal, presence: true
+  validates :product_price, :subtotal, numericality: { greater_than_or_equal_to: 1.0 }
 end
